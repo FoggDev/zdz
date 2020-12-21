45aa6d8774d8b29f24dc3d79de111c7a45fd5d56
Carlos S <js.education2020@gmail.com>
2020-12-20 19:04:03 -0800

    Testing button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,29 +1,3 @@
-import React from 'react'
-import classNamesGenerator from '../../theme/classNamesGenerator'
-import { COMPONENT_CLASS_NAME, ButtonBase, Sizes, Variants, Colors } from './styles'
-import { Theme } from '../../theme/theme'
+import { ButtonBase } from './styles'
 
-interface Props extends React.ComponentPropsWithoutRef<'button'> {
-  size?: Sizes
-  variant?: Variants
-  color?: Colors
-  theme?: Theme
-}
-
-const Button: React.FC<Props> = props => {
-  const { children, size = 'md', variant = 'contained', color = 'primary', ...btnProps } = props
-  const classNames = classNamesGenerator({
-    cpn: 'Button',
-    ccn: COMPONENT_CLASS_NAME,
-    data: [size, variant, color]
-  })
-  console.log('PROPS====>>>>', props)
-  console.log('btnProps====', btnProps)
-  return (
-    <ButtonBase className={classNames} {...btnProps}>
-      {children}
-    </ButtonBase>
-  )
-}
-
-export default Button
+export default ButtonBase
