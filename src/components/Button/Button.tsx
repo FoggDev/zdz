dd25ff39321c0da666c70bd6af0b848c3f7ce2a2
Carlos S <js.education2020@gmail.com>
2020-12-21 00:27:12 -0800

    Clean up

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,33 +1,21 @@
 import React, { FC } from 'react'
-import { defaultTheme } from '../../theme'
 import classNamesGenerator from '../../theme/classNamesGenerator'
 import { COMPONENT_CLASS_NAME, ButtonBase, Sizes, Variants, Colors } from './styles'
 
 interface Props extends React.ComponentPropsWithoutRef<'button'> {
-  theme?: any
   size?: Sizes
   variant?: Variants
   color?: Colors
 }
 
-const Button: FC<Props> = ({
-  children,
-  theme,
-  size = 'md',
-  variant = 'contained',
-  color = 'primary'
-}) => {
+const Button: FC<Props> = ({ children, size = 'md', variant = 'contained', color = 'primary' }) => {
   const classNames = classNamesGenerator({
     cpn: 'Button',
     ccn: COMPONENT_CLASS_NAME,
     data: [size, variant, color]
   })
 
-  return (
-    <ButtonBase theme={theme} className={classNames}>
-      {children}
-    </ButtonBase>
-  )
+  return <ButtonBase className={classNames}>{children}</ButtonBase>
 }
 
 export default Button
