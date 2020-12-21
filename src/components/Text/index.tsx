89491bf43b296821f25d934293635a764014e62a
Carlos S <js.education2020@gmail.com>
2020-12-20 18:47:37 -0800

    Adding Text component

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled'
+
+const Text = styled.div<any>`
+  color: ${({ theme }) => theme.palette.primary.main};
+`
+
+export default Text
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Theme } from './theme/theme'
 
 export { default as Button } from './components/Button'
+export { default as Text } from './components/Text'
 export { default as defaultTheme } from './theme/theme'
 export { Theme }
